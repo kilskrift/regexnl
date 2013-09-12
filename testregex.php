5121c4d9b5673eefc1f80a31a106ac4b35643a6a
Kristian Madsen <kristian.madsen@sveaekonomi.se>
2013-09-12 08:43:21 +0200

    Now supports international characters for Svea countries. Oops.

--- testregex.php
+++ testregex.php
@@ -1,3 +1,9 @@
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Test of regex</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+</head>
+<body>
 <?php 
 /** 
  * regex for handling NL address examples
@@ -9,13 +15,15 @@ error_reporting( E_ALL );
 
 $groups = Array();
 
-$pattern = "/^(?:\s)*([0-9]*[A-Za-z]*\s*[A-Za-z]+)(?:\s*)([0-9]*\s*[A-Za-z]*[^\s])?(?:\s)*$/";
+// split address into 2 groups, matching from start/end
+$pattern = "/^(?:\s)*([0-9]*[A-ZÄÅÆÖØÜßäåæöøüa-z]*\s*[A-ZÄÅÆÖØÜßäåæöøüa-z]+)(?:\s*)([0-9]*\s*[A-ZÄÅÆÖØÜßäåæöøüa-z]*[^\s])?(?:\s)*$/";
+//$pattern = "/^(?:\s)*([0-9]*\p{L}*\s*\p{L}+)(?:\s*)([0-9]*\s*\p{L}*[^\s])?(?:\s)*$/";     //if unicode support
 
 // leaves multiple spaces within addresses intact for now
 
 $testcases = Array();
 
-// testcases
+// testcases$
 $testcases["Street"] = Array("Street");
 $testcases["Street 10"] = Array("Street","10");
 $testcases[" Street 10 "] = Array("Street","10");
@@ -33,12 +41,19 @@ $testcases["3e laan 12"] = Array("3e laan","12");
 $testcases["Heeregracht 12bis"] = Array("Heeregracht","12bis");;
 //$testcases["slagen 7e 10" not matched, should be ok though, as never seen yet
 
+// international characters
+$testcases["Önskevägen 10"] = Array("Önskevägen","10");
+$testcases["Ålandshav 10å"] = Array("Ålandshav","10å");
+$testcases["Åväg änna 10"] = Array("Åväg änna","10");
+$testcases["ÄÅÖåäöÜü"] = Array("ÄÅÖåäöÜü");
+$testcases["ÄÅÆÖØÜßäåæöøü 10"] = Array("ÄÅÆÖØÜßäåæöøü","10");
+
 $show_all = false;
 
 echo "<pre>";
 foreach( $testcases as $given => $expected ) { 
 	preg_match( $pattern, $given, $groups );
-	echo( "given: '" . $given ."' \nexpected: \n  ");
+	echo( "given: \n  '" . $given ."' \nexpected: \n  ");
 	foreach( $expected as $group ) {
 		echo "'" . $group . "' ";
 	}
@@ -50,3 +65,5 @@ foreach( $testcases as $given => $expected ) {
 }
 echo "</pre>";
 ?>
+</body>
+</html>
